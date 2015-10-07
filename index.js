5f90090394748ab2bb922a49da4944975bb84a3c
David Ernst <git@dsernst.com>
2015-10-06 21:49:09 -0700

    Cleanup server debug code

--- index.js
+++ index.js
@@ -14,8 +14,5 @@ app.post('/', function (req, res) {
 })
 
 var server = app.listen(3000, function () {
-  var host = server.address().address
-  var port = server.address().port
-
-  console.log('Example app listening at http://%s:%s', host, port)
+  console.log('Server listening at http://' + server.address().address + ':' + server.address().port)
 })
