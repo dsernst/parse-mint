7225d64e3e79f53e386214f7167cb78adca78c4d
David Ernst <git@dsernst.com>
2015-10-06 22:50:53 -0700

    Lookup users by email address

--- parse.js
+++ parse.js
@@ -1,3 +1,6 @@
+var _ = require('lodash')
+var users = _.indexBy(require('./users.js'), 'emailAddress')
+
 function parseEmail(reqBody) {
   var emailAddress = reqBody.sender
   var body = reqBody['body-plain']
@@ -7,6 +10,18 @@ function parseEmail(reqBody) {
   var weeklySpent = body.split(weeklySpentPrefix)[1].split(weeklySpentSuffix)[0]
   console.log('emailAddress:', emailAddress)
   console.log('weeklySpent:', weeklySpent)
+  getStats(emailAddress, weeklySpent)
+}
+
+function getStats(emailAddress, weeklySpent) {
+  var user = users[emailAddress]
+
+  if (_.isUndefined(user)) {
+    console.error('User ' + emailAddress + ' not found.')
+    return
+  }
+
+  console.log('user:', user)
 }
 
 parseEmail(require('./sample-body.js'))
