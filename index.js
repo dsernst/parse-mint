f4f5926babe115058647a996f61ad13c048fca22
David Ernst <git@dsernst.com>
2015-10-06 23:43:12 -0700

    Add basic GET / routing

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var handleEmail = require('./handle-email.js')
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('<h3>Sign up for an automatic text message with your weekly savings percentage, based on <a href="http://www.mrmoneymustache.com/2012/01/13/the-shockingly-simple-math-behind-early-retirement" target="_blank">The Shockingly Simple Math Behind Early Retirement</a>.</h3><p>Send an email to <a href="mailto:parse-mint@dsernst.com">parse-mint@dsernst.com</a> to get started.</p>')
 })
 
 app.post('/', function (req, res) {
