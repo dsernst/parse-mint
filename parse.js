d50e5d6f0803bb67b88aec25d6282338e1ae56ee
David Ernst <git@dsernst.com>
2015-10-06 22:02:44 -0700

    Parse out users email address and weekly dollars spent

--- parse.js
+++ parse.js
@@ -1,1 +1,12 @@
-console.log(require('./sample-body.js'))
+function parseEmail(reqBody) {
+  var emailAddress = reqBody.sender
+  var body = reqBody['body-plain']
+  // console.log(body)
+  var weeklySpentPrefix = 'You spent $'
+  var weeklySpentSuffix = ' over the past 7 days.'
+  var weeklySpent = body.split(weeklySpentPrefix)[1].split(weeklySpentSuffix)[0]
+  console.log('emailAddress:', emailAddress)
+  console.log('weeklySpent:', weeklySpent)
+}
+
+parseEmail(require('./sample-body.js'))
